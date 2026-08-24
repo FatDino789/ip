cf6ab3ceb384d1ab1ecbcf7516119060056a3fde
FatDino789 <yeozk229@gmail.com>
2026-08-24 19:45:59 +0800

    Level-0

---
+++ src/main/java/Percy.java
@@ -0,0 +1,24 @@
+public class Percy {
+    public static void main(String[] args) {
+        // Chat Bot Banner
+        String banner = " ____                        \n"
+                + "|  _ \\ ___ _ __ ___ _   _ \n"
+                + "| |_) / _ \\ '__/ __| | | |\n"
+                + "|  __/  __/ | | (__| |_| |\n"
+                + "|_|   \\___|_|  \\___|\\__, |\n"
+                + "                    |___/ \n";
+
+        // Horizontal Strings for Design
+        String line = "____________________________________________________________";
+
+        // Greet the User and Exit
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Percy.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
