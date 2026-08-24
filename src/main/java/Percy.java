00af8e073785805a8a9890c5c05e48e60f91ac12
FatDino789 <yeozk229@gmail.com>
2026-08-24 19:53:17 +0800

    Level-1

--- src/main/java/Percy.java
+++ src/main/java/Percy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Percy {
     public static void main(String[] args) {
         // Chat Bot Banner
@@ -11,14 +13,29 @@ public class Percy {
         // Horizontal Strings for Design
         String line = "____________________________________________________________";
 
-        // Greet the User and Exit
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
         System.out.println(banner);
-        System.out.println("Hello! I'm Percy.");
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        // Continuously prompt until the user inputs bye
+        while (true) {
+            // Take in the user's input
+            input = scanner.nextLine();
+
+            // Check condition that the user inputted bye
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
     }
 }
